4a70702cc6e739db78f3c91ac7f1b925cd0139e6
Mdkaif-123 <amdkaif843@gmail.com>
2023-08-13 01:02:43 +0530

    First commit

---
+++ Projects/Vb.net Practise/Vb.net Practise/Pr-1_variable.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+
+        Dim sCarName As String
+        Dim dCarPrice As Double
+        Dim sCarColor As String
+        Dim bTax As Boolean
+        Dim dFuelCostPerYear As Decimal
+
+        sCarName = "Toyota"
+        dCarPrice = 12000000
+        sCarColor = "Black"
+        bTax = True
+        dFuelCostPerYear = 99.9
+
+
+
+        MsgBox("My car is " & sCarName & dCarPrice & sCarColor & bTax & dFuelCostPerYear)
+
+
+
+
+    End Sub
+
+End Class
---
+++ Projects/Vb.net Practise/Vb.net Practise/Pr-2_input.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form2
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+
+        Dim firstName As String
+        Dim lastName As String
+        Dim gender As String
+        Dim occupation As String
+
+
+        firstName = TextBox1.Text
+        lastName = TextBox2.Text
+        gender = TextBox3.Text
+        occupation = ListBox1.SelectedItem
+
+        MsgBox("Hey , " & firstName & " " & lastName & ". You are a " & occupation)
+
+    End Sub
+End Class
